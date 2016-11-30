fdacf1dff46a67ebbb1d580a0a3e84e91626b433
Joe Andolina <jandolina@jandolina-ltm.internal.salesforce.com>
2016-11-29 17:35:45 -0800

    A little house keeping

--- Demo-Swift/slds-sample-app/FontListTableViewController.swift
+++ Demo-Swift/slds-sample-app/FontListTableViewController.swift
@@ -12,12 +12,15 @@ class FontListTableViewController: UITableViewController {
     
     var fontTypes = Array<String>();
     var fontSizes = Array<String>();
+  
+    //––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Fonts"
         self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "fontCell")
         
+        // NOTE: Collecting all the Font Names
         repeat {
             if let fontType = SLDSFontType.init(rawValue: fontTypes.count),
                 let fontName = SLDSFont.sldsFontTypeName(fontType) {
@@ -25,6 +28,7 @@ class FontListTableViewController: UITableViewController {
             }
         } while SLDSFontType.init(rawValue: fontTypes.count)?.hashValue != 0
         
+        // NOTE: Collecting all the Font Sizes
         repeat {
             if let fontSize = SLDSFontSizeType.init(rawValue: fontSizes.count),
                 let sizeName = SLDSFont.sldsFontSizeName(fontSize) {
@@ -33,28 +37,39 @@ class FontListTableViewController: UITableViewController {
         } while SLDSFontSizeType.init(rawValue: fontSizes.count)?.hashValue != 0
     }
 
+    //––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
     // MARK: - Table view data source
-
+    //––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return fontTypes.count
     }
 
+    //––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return fontSizes.count
     }
+    
+    //––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
     
     override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 30.0
     }
     
+    //––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+    
     override func tableView(_ tableView: UITableView, indentationLevelForRowAt indexPath: IndexPath) -> Int {
         return 2
     }
+    
+    //––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
     
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let header = UITableViewHeaderFooterView.init()
@@ -62,56 +77,13 @@ class FontListTableViewController: UITableViewController {
         return header
     }
     
+    //––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "fontCell", for: indexPath)
         cell.textLabel?.text = fontSizes[indexPath.row]
         cell.textLabel?.font = UIFont.sldsFont(SLDSFontType.init(rawValue: indexPath.section)!, with:SLDSFontSizeType.init(rawValue:indexPath.row)!)
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
