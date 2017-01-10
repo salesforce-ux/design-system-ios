1c8850e28076620e36500740bb33a6e01e07ebd3
jandolina <jandolina@salesforce.com>
2017-01-10 12:43:00 -0800

    Started integrating colors

---
+++ SalesforceDesignSystem/Generated/Extensions/UIColor+SLDSColor.m
@@ -0,0 +1,19 @@
+#import "UIColor+SLDSColor.h"
+
+@implementation UIColor (SLDSColor)
+
+
++(instancetype)sldsFill:(SLDSFillType)colorType{
+    return [SLDSColor sldsFill:colorType];
+}
++(instancetype)sldsBorderColor:(SLDSBorderColorType)colorType{
+    return [SLDSColor sldsBorderColor:colorType];
+}
++(instancetype)sldsBackgroundColor:(SLDSBackgroundColorType)colorType{
+    return [SLDSColor sldsBackgroundColor:colorType];
+}
++(instancetype)sldsTextColor:(SLDSTextColorType)colorType{
+    return [SLDSColor sldsTextColor:colorType];
+}
+
+@end
